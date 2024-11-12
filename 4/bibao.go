0c6e37f4d9672ab08ad85c35ca08a51e90922460
yu666yu666yu666 <1759753124@qq.com>
2024-11-12 13:05:21 +0800

    test

--- 4/bibao.go
+++ 4/bibao.go
@@ -20,17 +20,22 @@ func calc(base int) (func(int) int, func(int) int) {
 func dispatchCoin() int {
 	totolcoins := coins
 
-	rule := map[rune]int{
+	rules := map[rune]int{
 		'e': 1, 'E': 1,
 		'i': 2, 'I': 2,
 		'o': 3, 'O': 3,
 		'u': 4, 'U': 4,
 	}
 
+	//rules[letter]：尝试从 rules 映射中获取 letter 对应的值。
+	//value：如果 letter 存在于 rules 中，则 value 将赋值为对应的金币数。
+	//exists：这是一个布尔值，表示 letter 是否在 rules 中存在：
+	//	如果存在，exists 为 true，并且 value 将是对应的金币数。
+	//	如果不存在，exists 为 false，value 将为该类型的零值（在此情况下为 0）。
 	for _, user := range users {
 		usercoins := 0
-		for _, userchar := range user {
-			if value, exists := rule[userchar]; exists {
+		for _, letter := range user {
+			if value, exists := rules[letter]; exists {
 				usercoins += value
 			}
 		}
