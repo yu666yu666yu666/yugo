d40e558f6125ac0368ee8f0f8339fa599d17be99
yu666yu666yu666 <1759753124@qq.com>
2024-11-12 13:00:36 +0800

    test

--- 4/bibao.go
+++ 4/bibao.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func calc(base int) (func(int) int, func(int) int) {
 
 	add := func(i int) int {
@@ -13,4 +15,30 @@ func calc(base int) (func(int) int, func(int) int) {
 	}
 
 	return add, sub
+}
+
+func dispatchCoin() int {
+	totolcoins := coins
+
+	rule := map[rune]int{
+		'e': 1, 'E': 1,
+		'i': 2, 'I': 2,
+		'o': 3, 'O': 3,
+		'u': 4, 'U': 4,
+	}
+
+	for _, user := range users {
+		usercoins := 0
+		for _, userchar := range user {
+			if value, exists := rule[userchar]; exists {
+				usercoins += value
+			}
+		}
+
+		fmt.Println(usercoins)
+		distribrtion[user] = usercoins
+		totolcoins -= usercoins
+	}
+
+	return totolcoins
 }
--- 4/main.go
+++ 4/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+var (
+	coins = 50
+	users = []string{
+		"Matthew", "Sarah", "Augustus", "Heidi", "Emilie", "Peter", "Giana", "Adriano", "Aaron", "Elizabeth",
+	}
+	distribrtion = make(map[string]int, len(users))
+)
+
 func main() {
 
 	//共用一个base
@@ -10,4 +18,7 @@ func main() {
 	fmt.Println(f1(3), f2(4))
 	fmt.Println(f1(5), f2(6))
 	fmt.Println(f1(7), f2(8))
+
+	left := dispatchCoin()
+	fmt.Println("剩下:", left)
 }
