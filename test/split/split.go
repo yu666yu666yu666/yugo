9343aa00685f0f9d0859504d6004f31505e899a5
yu666yu666yu666 <1759753124@qq.com>
2024-11-14 21:03:37 +0800

    单元测试

--- test/split/split.go
+++ test/split/split.go
@@ -7,8 +7,8 @@ import "strings"
 // Split slices s into all substrings separated by sep and
 // returns a slice of the substrings between those separators.
 func Split(s, sep string) (result []string) {
+	result = make([]string, 0, strings.Count(s, sep)+1)
 	i := strings.Index(s, sep)
-
 	for i > -1 {
 		result = append(result, s[:i])
 		s = s[i+len(sep):] // 这里使用len(sep)获取sep的长度
@@ -17,3 +17,5 @@ func Split(s, sep string) (result []string) {
 	result = append(result, s)
 	return
 }
+
+//使用make函数提前分配内存的改动，减少了2/3的内存分配次数，并且减少了一半的内存分配。
--- test/split/split_test.go
+++ test/split/split_test.go
@@ -1,9 +1,26 @@
 package split
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
+
+// 测试集的Setup与Teardown
+func setupTestCase(t *testing.T) func(t *testing.T) {
+	t.Log("如有需要在此执行:测试之前的setup")
+	return func(t *testing.T) {
+		t.Log("如有需要在此执行:测试之后的teardown")
+	}
+}
+
+// 子测试的Setup与Teardown
+func setupSubTest(t *testing.T) func(t *testing.T) {
+	t.Log("如有需要在此执行:子测试之前的setup")
+	return func(t *testing.T) {
+		t.Log("如有需要在此执行:子测试之后的teardown")
+	}
+}
 
 func TestSplit(t *testing.T) {
 	type test struct { // 定义test结构体
@@ -17,8 +34,13 @@ func TestSplit(t *testing.T) {
 		"more sep":    {input: "abcd", sep: "bc", want: []string{"a", "d"}},
 		"leading sep": {input: "沙河有沙又有河", sep: "沙", want: []string{"", "河有", "又有河"}},
 	}
+	teardownTestCase := setupTestCase(t) // 测试之前执行setup操作
+	defer teardownTestCase(t)            // 测试之后执行testdoen操作
+
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) { // 使用t.Run()执行子测试
+			teardownSubTest := setupSubTest(t) // 子测试之前执行setup操作
+			defer teardownSubTest(t)           // 测试之后执行testdoen操作
 			got := Split(tc.input, tc.sep)
 			if !reflect.DeepEqual(got, tc.want) {
 				t.Errorf("expected:%#v, got:%#v", tc.want, got)
@@ -36,3 +58,17 @@ func TestMoreSplit(t *testing.T) {
 	}
 }
 */
+
+func BenchmarkSplit(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Split("沙河有沙又有河", "沙")
+	}
+}
+
+func ExampleSplit() {
+	fmt.Println(Split("a:b:c", ":"))
+	fmt.Println(Split("沙河有沙又有河", "沙"))
+	// Output:
+	// [a b c]
+	// [ 河有 又有河]
+}
